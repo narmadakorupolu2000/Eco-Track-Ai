b746efaa1848963a7d97615f94ed96982bf3cfc4
narmadakorupolu2000 <narmadakorupolunarmada37205@gmail.com>
2025-11-03 19:19:05 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -59,7 +59,7 @@ app = FastAPI(
 # --- Middleware ---
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=FRONTEND_ORIGINS,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
